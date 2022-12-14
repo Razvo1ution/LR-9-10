e3f4904ce407e7e1aa8c894da41b37f58fd91c50
Razvo1ution <117993907+Razvo1ution@users.noreply.github.com>
2022-12-14 12:51:48 +0300

    Add files via upload

---
+++ лаб9/з1 Л9.pas
@@ -0,0 +1,15 @@
+﻿function pm(p,p1: integer) :=p*p1;
+const n=5;
+var a:array[1..n] of integer;
+   i,p,p1,p2:integer;
+ begin
+for i:=1 to n do
+ readln(a[i]);
+p:=1;
+  for i:=1 to n do
+   if i mod 2=0 then begin
+     p1:=a[i];
+    p:=pm(p,p1);
+    end;
+  Writeln(p);
+ end.
---
+++ лаб9/з2 Л9.pas
@@ -0,0 +1,19 @@
+﻿var a:array[1..7] of integer;
+   i,g,p,n,min:integer;
+ begin
+   n:=7;
+for i:=1 to n do
+ readln(a[i]);
+p:=1;
+  for i:=2 to n do
+   if abs(a[i]) < abs(a[i-1]) then
+    p:=i;
+   min:=1;
+   for i:=2 to n do
+     if a[i]<0 then if a[min]>a[i] then min:=i;
+  writeln('Минимальный по модулю элемент ', p, ' ', abs(a[p]));
+  writeln('Максимальный отрицательный элемент ', min, ' ', a[min]);
+ end;
+begin
+  pr;
+  end.
